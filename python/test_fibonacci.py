a7c82f685ab69296379b3a32db826a893f16802f
lukeconibear <dsSvBCGltnC!j4Fp8MZ8>
2020-11-10 15:23:56 +0000

    Renaming tests

--- python/test_fibonacci.py
+++ python/test_fibonacci.py
@@ -3,10 +3,11 @@ import fibonacci
 
 
 class TestCalc(unittest.TestCase):
-    def test_fibonacci(self):
+    def test_fib_recursive(self):
         self.assertEqual(fibonacci.fib_recursive(6), 5)
         self.assertEqual(fibonacci.fib_recursive(10), 34)
 
+    def test_fib_iterative(self):
         self.assertEqual(fibonacci.fib_iterative(6), 5)
         self.assertEqual(fibonacci.fib_iterative(10), 34)
 
