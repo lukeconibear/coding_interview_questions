9467ec3c2edd7561b5273fd15fc2e15271a54f6a
lukeconibear <dsSvBCGltnC!j4Fp8MZ8>
2020-11-10 14:51:21 +0000

    Trying renaming test file

--- python/tests.py
+++ python/tests.py
@@ -3,7 +3,7 @@ import fibonacci
 
 
 class TestCalc(unittest.TestCase):
-    def test_add(self):
+    def test_fibonacci(self):
         self.assertEqual(fibonacci.fib_recursive(6), 5)
         self.assertEqual(fibonacci.fib_recursive(10), 34)
 
