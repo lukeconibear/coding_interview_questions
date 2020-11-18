9461e539bc3765ed53948596ff60127def173566
Luke Conibear <laconibear@gmail.com>
2020-11-18 12:57:54 +0000

    refactoring folders for multiple languages/formats

--- tests/python/test_fibonacci.py
+++ tests/python/test_fibonacci.py
@@ -2,8 +2,8 @@ import os
 import sys
 import unittest
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from questions import fibonacci
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from questions.python import fibonacci
 
 
 class TestCalc(unittest.TestCase):
--- tests/python/test_two_sum.py
+++ tests/python/test_two_sum.py
@@ -2,8 +2,8 @@ import os
 import sys
 import unittest
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from questions import two_sum
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from questions.python import two_sum
 
 
 class Test(unittest.TestCase):
